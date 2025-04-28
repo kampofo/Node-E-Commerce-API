79b0c9aead77de5cacfcd5b8288763597e074474
Kwabena Ampofo <amp.kwabena@gmail.com>
2025-04-28 01:18:00 -0400

    Add admin routes

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post('/products', (req: Request, res: Response) => {
+    // TODO: ADD LOGIC FOR ROUTE
+    res.send("route reached");
+});
+
+router.get('/products', (req: Request, res: Response) => {
+    // TODO: ADD LOGIC FOR ROUTE
+    res.send("route reached");
+});
+
+router.put('/products/:id', (req: Request, res: Response) => {
+    // TODO: ADD LOGIC FOR ROUTE
+    res.send("route reached");
+});
+
+router.delete('/products/:id', (req: Request, res: Response) => {
+    // TODO: ADD LOGIC FOR ROUTE
+    res.send("route reached");
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express, { Express } from "express";
 
+import adminRoutes from '../src/routes/admin.routes';
+
 const app: Express = express();
 
+// app middlewares
 app.use(express.json());
+
+// routing middlewares
+app.use('/api/admin', adminRoutes);
 
 app.get('/', (req, res) => {
 	res.send("<h1>Hello from TypeScript!</h1>");
